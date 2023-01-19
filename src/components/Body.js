443afffcddb8c91a4314972352307754d93122d8
assiri <assiri@users.noreply.github.com>
2023-01-19 06:37:06 +0000

    deploy: 325ef3b4b42f9ee2aa833167d412dec31688a4f3

--- src/components/Body.js
+++
@@ -1,9 +0,0 @@
-export default function Body({ children, ...props }) {
-    return (
-        <div className="flex flex-dc px-auto" style={{ alignContent: "center" }}  {...props}>
-            <div style={{ margin: "auto", maxWidth: '1000px' }}>
-                {children}
-            </div>
-        </div>
-    )
-}
--- src/utils/contentful/CF.js
+++
@@ -1,7 +0,0 @@
-import * as contentful from 'contentful'
-
-const CF = contentful.createClient({
-    space: `${process.env.SPACE_ID}`,
-    accessToken: `${process.env.ACCESS_TOKEN}`
-})
-export default CF;
